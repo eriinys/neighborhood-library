ba557cdcbade82e16829914b078071dd08acd226
eriinys <eriinys@duck.com>
2025-10-04 18:33:47 -0500

    Added methods and toString. Added second constructor for flexibility

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -2,16 +2,21 @@ public class Book {
     private int id;
     private String isbn;
     private String title;
-    private boolean isCheckedOut;
+    private boolean checkedOut;
     private String checkedOutTo;
 
+    //region constructor
     public Book (int id, String isbn, String title, boolean isCheckedOut, String checkedOutTo) {
         this.id = id;
         this.isbn = isbn;
         this.title = title;
-        this.isCheckedOut = isCheckedOut;
-        this.checkedOutTo   = checkedOutTo;
+        this.checkedOut = isCheckedOut;
+        this.checkedOutTo = checkedOutTo;
     }
+        public Book(int id, String isbn, String title){
+            this(id, isbn, title, false, "");
+        }
+    //endregion
 
 //region getters
 public int getId() {
@@ -35,19 +40,47 @@ public void setTitle(String title){
         this.title = title;
 }
 
-public boolean getIsCheckedOut(){
-        return  isCheckedOut;
+public boolean isCheckedOut(){
+        return  checkedOut;
 }
-public void setIsCheckedOut(boolean isCheckedOut){
-        this.isCheckedOut = isCheckedOut;
+public void setCheckedOut(boolean checkedOut){
+        this.checkedOut = checkedOut;
 }
 
 public String getCheckedOutTo(){
         return checkedOutTo;
 }
-public  void setCheckedOut(String checkedOutTo){
-        this.checkedOutTo = checkedOutTo;
+public  void setCheckedOutTo(String checkedOutTo){
+        this.checkedOutTo = (checkedOutTo == null) ? "" : checkedOutTo;
 }
 //endregion
 
+
+//region method
+public boolean checkOutName(String name) {
+    String borrower = (name == null) ? "" : name;
+    if (borrower.isBlank() || this.checkedOut) {
+        return false;
+    }
+    this.checkedOutTo = borrower;
+    this.checkedOut = true;
+    return true;
+}
+
+public boolean checkIn(){
+        if(!this.checkedOut)
+            return false;
+        this.checkedOutTo = "";
+        this.checkedOut = false;
+        return true;
+}
+
+//endregion
+
+
+//toString
+public String toString(){
+        String status = checkedOut ? String.format("Currently checked out to %s", checkedOutTo) : "Available";
+        return String.format("#%d | \"%s\" (ISBN %s) - %s", id, title, isbn, status);
+}
 }
---
+++ src/main/java/Library.java
@@ -0,0 +1,4 @@
+public class Library {
+    private static Book[] inventory = new Book[20];
+
+}
